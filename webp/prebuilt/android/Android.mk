8ceb51627dd8ab192ca2eeda6b09a2db1b4dfc79
minggo <ming@cocos2d-x.org>
2014-08-01 15:45:52 +0800

    add webp as a module

--- webp/prebuilt/android/Android.mk
+++ webp/prebuilt/android/Android.mk
@@ -1,8 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
+# wep lib
 include $(CLEAR_VARS)
-LOCAL_MODULE := cocos_webp_static
-LOCAL_MODULE_FILENAME := webp
+LOCAL_MODULE := webp_static
+LOCAL_MODULE_FILENAME := webp_internal
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebp.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
 
@@ -15,3 +16,15 @@ endif
 include $(PREBUILT_STATIC_LIBRARY)
 
 $(call import-module, android/cpufeatures)
+
+# web module
+include $(CLEAR_VARS)
+LOCAL_MODULE := cocos_webp_static
+LOCAL_MODULE_FILENAME := webp
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../CCModuleWEP.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../cocos \
+                    $(LOCAL_PATH)/../../../../cocos/platform/android \
+                    $(LOCAL_PATH)/../../include/android
+
+include $(BUILD_STATIC_LIBRARY)
