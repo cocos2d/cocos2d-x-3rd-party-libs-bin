1d608d88f90f7c284f9d27ac4a349f08a058db02
minggo <ming@cocos2d-x.org>
2014-10-20 14:55:16 +0800

    Update Android.mk

--- protobuf-lite/Android.mk
+++ protobuf-lite/Android.mk
@@ -18,7 +18,8 @@ src/google/protobuf/io/zero_copy_stream_impl.cc \
 src/google/protobuf/io/zero_copy_stream_impl_lite.cc \
 src/google/protobuf/stubs/common.cc \
 src/google/protobuf/stubs/once.cc \
-src/google/protobuf/stubs/stringprintf.cc
+src/google/protobuf/stubs/stringprintf.cc \
+src/google/protobuf/stubs/atomicops_internals_x86_gcc.cc
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/src
 
