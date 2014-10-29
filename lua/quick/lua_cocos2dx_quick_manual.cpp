999f595850493816ce3b6af2d345874f3766f370
SunLightJuly <ygqy8181@qq.com>
2014-10-29 16:02:01 +0800

    added cc. LuaLoadChunksFromZIP()

--- lua/quick/lua_cocos2dx_quick_manual.cpp
+++ lua/quick/lua_cocos2dx_quick_manual.cpp
@@ -400,11 +400,25 @@ static void extendNode(lua_State* tolua_S)
     lua_pop(tolua_S, 1);
 }
 
+static int tolua_Cocos2d_Function_loadChunksFromZIP(lua_State* tolua_S)
+{
+    return LuaEngine::getInstance()->getLuaStack()->luaLoadChunksFromZIP(tolua_S);
+}
+
+static void extendFunctions(lua_State* tolua_S)
+{
+    tolua_module(tolua_S,"cc",0);
+    tolua_beginmodule(tolua_S,"cc");
+    tolua_function(tolua_S,"LuaLoadChunksFromZIP",tolua_Cocos2d_Function_loadChunksFromZIP);
+    tolua_endmodule(tolua_S);
+}
+
 int register_all_quick_manual(lua_State* tolua_S)
 {
     if (nullptr == tolua_S)
         return 0;
     
+    extendFunctions(tolua_S);
     extendNode(tolua_S);
 
     CCLOG("Quick-Cocos2d-x C++ support ready.");
