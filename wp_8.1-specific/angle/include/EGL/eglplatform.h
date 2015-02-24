51c70058c3933ee1a79626c938243be1184217fc
Dale Stammen <dalestam@microsoft.com>
2015-02-24 09:32:25 -0800

    updated angle to commit 408bfedc8a7febc41a0c1036261992cbc8a3cae7

--- wp_8.1-specific/angle/include/EGL/eglplatform.h
+++ wp_8.1-specific/angle/include/EGL/eglplatform.h
@@ -76,11 +76,11 @@
 typedef HDC     EGLNativeDisplayType;
 typedef HBITMAP EGLNativePixmapType;
 
-#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_PC_APP || WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP) /* Windows Store */
+#if !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP) /* Windows Desktop */
+typedef HWND    EGLNativeWindowType;
+#else /* Windows Store */
 #include <inspectable.h>
 typedef IInspectable* EGLNativeWindowType;
-#else
-typedef HWND    EGLNativeWindowType;
 #endif
 
 #elif defined(__WINSCW__) || defined(__SYMBIAN32__)  /* Symbian */
@@ -108,6 +108,14 @@ typedef void*                           EGLNativeDisplayType;
 typedef Display *EGLNativeDisplayType;
 typedef Pixmap   EGLNativePixmapType;
 typedef Window   EGLNativeWindowType;
+
+#elif defined(__GNUC__) && ( defined(__APPLE_CPP__) || defined(__APPLE_CC__) || defined(__MACOS_CLASSIC__) )
+
+// TODO(jmadill): native implementation for OSX
+
+typedef void *EGLNativeDisplayType;
+typedef void *EGLNativePixmapType;
+typedef void *EGLNativeWindowType;
 
 #else
 #error "Platform not recognized"
