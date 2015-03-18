68a2724293a34b74f32385ce4f3fedcc153a9eee
andyque <guanghui8827@126.com>
2015-03-18 19:32:15 +0800

    sync cocos2d-x-libs-src

--- android/Android.mk
+++ android/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_chipmunk_static
 LOCAL_MODULE_FILENAME := chipmunk
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libchipmunk.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #config for box2d
@@ -12,7 +11,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_box2d_static
 LOCAL_MODULE_FILENAME := box2d
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libbox2d.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #config for cjson
@@ -20,7 +18,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_cjson_static
 LOCAL_MODULE_FILENAME := cjson
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libcjson.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #config for convertutf
@@ -28,7 +25,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_convertutf_static
 LOCAL_MODULE_FILENAME := convertutf
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libconvertutf.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #config for curl
@@ -36,7 +32,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_curl_static
 LOCAL_MODULE_FILENAME := curl
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libcurl.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 LOCAL_STATIC_LIBRARIES += cocos_ssl_static
 LOCAL_STATIC_LIBRARIES += cocos_crypto_static
 include $(PREBUILT_STATIC_LIBRARY)
@@ -58,15 +53,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_edtaa3func_static
 LOCAL_MODULE_FILENAME := edtaa3func
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libedtaa3func.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
-include $(PREBUILT_STATIC_LIBRARY)
-
-#flatbuffers
-include $(CLEAR_VARS)
-LOCAL_MODULE := cocos_flatbuffers_static
-LOCAL_MODULE_FILENAME := flatbuffers
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libflatbuffers.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #freetype2
@@ -74,8 +60,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_freetype2_static
 LOCAL_MODULE_FILENAME := freetype2
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libfreetype.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include \
-			    $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/freetype
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/freetype
 include $(PREBUILT_STATIC_LIBRARY)
 
 #jpeg
@@ -83,7 +68,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_jpeg_static
 LOCAL_MODULE_FILENAME := jpeg
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libjpeg.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #png
@@ -91,7 +75,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_png_static
 LOCAL_MODULE_FILENAME := png
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libpng.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #tiff
@@ -99,7 +82,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_tiff_static
 LOCAL_MODULE_FILENAME := tiff
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libtiff.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #tinyxml
@@ -107,7 +89,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_tinyxml2_static
 LOCAL_MODULE_FILENAME := tinyxml2
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libtinyxml2.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #minizip
@@ -115,7 +96,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_minizip_static
 LOCAL_MODULE_FILENAME := minizip
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libminizip.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #webp
@@ -123,7 +103,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_webp_static
 LOCAL_MODULE_FILENAME := webp
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libwebp.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #websockets
@@ -131,7 +110,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_websockets_static
 LOCAL_MODULE_FILENAME := websockets
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libwebsockets.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 #xxhash
@@ -139,7 +117,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_xxhash_static
 LOCAL_MODULE_FILENAME := xxhash
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libxxhash.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 ##xxtea
@@ -147,7 +124,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_xxtea_static
 LOCAL_MODULE_FILENAME := xxtea
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libxxtea.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 ##zlib
@@ -155,7 +131,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_zlib_static
 LOCAL_MODULE_FILENAME := zlib
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libz.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 ##luasocket
@@ -163,7 +138,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_luasocket_static
 LOCAL_MODULE_FILENAME := luasocket
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/libluasocket.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 ##luajit
@@ -179,7 +153,7 @@ else
 LOCAL_MODULE := cocos_lua_static
 LOCAL_MODULE_FILENAME := lua
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libs/liblua.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/lua
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)/include/luajit
 endif
 include $(PREBUILT_STATIC_LIBRARY)
 
