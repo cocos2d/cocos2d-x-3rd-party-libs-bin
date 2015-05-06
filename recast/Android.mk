375a899aaaccbe56f3bb0424c2cef9ca7a593eda
songchengjiang <moses_jc@sina.com>
2015-05-06 16:29:18 +0800

    filter unused files

--- recast/Android.mk
+++ recast/Android.mk
@@ -9,32 +9,12 @@ LOCAL_MODULE_FILENAME := librecast
 LOCAL_SRC_FILES := \
 DebugUtils/DebugDraw.cpp \
 DebugUtils/DetourDebugDraw.cpp \
-DebugUtils/RecastDebugDraw.cpp \
-DebugUtils/RecastDump.cpp \
 Detour/DetourAlloc.cpp \
 Detour/DetourCommon.cpp \
 Detour/DetourNavMesh.cpp \
 Detour/DetourNavMeshBuilder.cpp \
 Detour/DetourNavMeshQuery.cpp \
-Detour/DetourNode.cpp \
-DetourCrowd/DetourCrowd.cpp \
-DetourCrowd/DetourLocalBoundary.cpp \
-DetourCrowd/DetourObstacleAvoidance.cpp \
-DetourCrowd/DetourPathCorridor.cpp \
-DetourCrowd/DetourPathQueue.cpp \
-DetourCrowd/DetourProximityGrid.cpp \
-DetourTileCache/DetourTileCache.cpp \
-DetourTileCache/DetourTileCacheBuilder.cpp \
-Recast/Recast.cpp \
-Recast/RecastAlloc.cpp \
-Recast/RecastArea.cpp \
-Recast/RecastContour.cpp \
-Recast/RecastFilter.cpp \
-Recast/RecastLayers.cpp \
-Recast/RecastMesh.cpp \
-Recast/RecastMeshDetail.cpp \
-Recast/RecastRasterization.cpp \
-Recast/RecastRegion.cpp
+Detour/DetourNode.cpp
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/..
 
