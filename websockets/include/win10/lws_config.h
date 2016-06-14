b928c4af2cabb2cc1243b7f6735461368f6de6d5
Dale Stammen <dalestam@microsoft.com>
2016-06-14 14:52:08 -0700

    Win10 UWP x64 builds. Update ANGLE to 2.1.9

--- websockets/include/win10/lws_config.h
+++ websockets/include/win10/lws_config.h
@@ -24,7 +24,7 @@
 #define LWS_LIBRARY_VERSION_NUMBER (LWS_LIBRARY_VERSION_MAJOR*1000000)+(LWS_LIBRARY_VERSION_MINOR*1000)+LWS_LIBRARY_VERSION_PATCH
 
 /* The current git commit hash that we're building from */
-#define LWS_BUILD_HASH "0db326a"
+#define LWS_BUILD_HASH "c26e93a"
 
 /* Build with OpenSSL support */
 /* #undef LWS_OPENSSL_SUPPORT */
