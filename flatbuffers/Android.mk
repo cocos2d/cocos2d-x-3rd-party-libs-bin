ba582063bea0a450c92c6323ffa736c04414e284
minggo <xiaoming.zhang@chukong-inc.com>
2017-06-15 11:04:17 +0800

    revert updating flatbuffers (#296)

--- flatbuffers/Android.mk
+++ flatbuffers/Android.mk
@@ -10,18 +10,12 @@ LOCAL_ARM_MODE := arm
 
 LOCAL_SRC_FILES := \
 flatc.cpp \
-flathash.cpp \
 idl_gen_cpp.cpp \
 idl_gen_fbs.cpp \
 idl_gen_general.cpp \
 idl_gen_go.cpp \
-idl_gen_js.cpp \
-idl_gen_php.cpp \
-idl_gen_python.cpp \
 idl_gen_text.cpp \
-idl_parser.cpp \
-reflection.cpp \
-util.cpp
+idl_parser.cpp
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/..
 
