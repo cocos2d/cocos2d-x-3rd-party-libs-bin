f00f3416cf3d2c0efedb5995c886791a7c5c74b7
minggo <ming@cocos2d-x.org>
2017-06-19 17:36:30 +0800

    update header path

--- bullet/prebuilt/android/Android.mk
+++ bullet/prebuilt/android/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := bullet_static
 LOCAL_MODULE_FILENAME := bullet_static
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libLinearMath.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
-LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../include/android/bullet
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/../../include/bullet
 LOCAL_STATIC_LIBRARIES += cocos_bulletcollision_static
 LOCAL_STATIC_LIBRARIES += cocos_bulletdynamics_static
 LOCAL_STATIC_LIBRARIES += cocos_bulletmultithreaded_static
@@ -18,7 +18,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_bulletcollision_static
 LOCAL_MODULE_FILENAME := bulletcollision
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libBulletCollision.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_STATIC_LIBRARIES += cocos_linearmath_static
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -26,7 +26,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_bulletdynamics_static
 LOCAL_MODULE_FILENAME := bulletdynamics
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libBulletDynamics.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_STATIC_LIBRARIES += cocos_bulletcollision_static
 LOCAL_STATIC_LIBRARIES += cocos_linearmath_static
 include $(PREBUILT_STATIC_LIBRARY)
@@ -35,19 +35,19 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_bulletmultithreaded_static
 LOCAL_MODULE_FILENAME := bulletmultithreaded
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libBulletMultiThreaded.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_linearmath_static
 LOCAL_MODULE_FILENAME := linearmath
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libLinearMath.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := cocos_minicl_static
 LOCAL_MODULE_FILENAME := minicl
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libMiniCL.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include/android
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../include
 include $(PREBUILT_STATIC_LIBRARY)
