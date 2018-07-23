04f27ee611ec7d0619ac248de75fe019da6ba3e7
halx99 <halx99@live.com>
2018-07-23 18:22:27 +0800

    [ANDROID] Refactor audio module, add android decoder libs.

---
+++ android-specific/MP3Decoder/prebuilt/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mpg123_shared
+LOCAL_MODULE_FILENAME := libmpg123
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libmpg123.so
+include $(PREBUILT_SHARED_LIBRARY)
---
+++ android-specific/OpenalSoft/prebuilt/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := openal_shared
+LOCAL_MODULE_FILENAME := libopenal
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libopenal.so
+include $(PREBUILT_SHARED_LIBRARY)
