4821e600be74ff4d11b831480e27a46a2c71b3f2
HALX99 <halx99@live.com>
2019-09-24 09:15:19 +0800

    Upgrade minizip to 1.2.0 (#375)

--- unzip/ioapi_mem.h
+++ unzip/ioapi_mem.h
@@ -1,61 +1,52 @@
+/* ioapi_mem.h -- IO base function header for compress/uncompress .zip
+   files using zlib + zip or unzip API
 
-#ifndef quickcocos2dx_ioapi_mem_h
-#define quickcocos2dx_ioapi_mem_h
+   This version of ioapi is designed to access memory rather than files.
+   We do use a region of memory to put data in to and take it out of.
 
+   Copyright (C) 2012-2017 Nathan Moinvaziri (https://github.com/nmoinvaz/minizip)
+             (C) 2003 Justin Fletcher
+             (C) 1998-2003 Gilles Vollant
+
+   This program is distributed under the terms of the same license as zlib.
+   See the accompanying LICENSE file for the full text of the license.
+*/
+
+#ifndef _IOAPI_MEM_H
+#define _IOAPI_MEM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "zlib.h"
 #include "ioapi.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-namespace cocos2d {
-    voidpf ZCALLBACK fopen_mem_func OF((
-                                        voidpf opaque,
-                                        const char* filename,
-                                        int mode));
+voidpf   ZCALLBACK fopen_mem_func(voidpf opaque, const char* filename, int mode);
+voidpf   ZCALLBACK fopendisk_mem_func(voidpf opaque, voidpf stream, uint32_t number_disk, int mode);
+uint32_t ZCALLBACK fread_mem_func(voidpf opaque, voidpf stream, void* buf, uint32_t size);
+uint32_t ZCALLBACK fwrite_mem_func(voidpf opaque, voidpf stream, const void* buf, uint32_t size);
+long     ZCALLBACK ftell_mem_func(voidpf opaque, voidpf stream);
+long     ZCALLBACK fseek_mem_func(voidpf opaque, voidpf stream, uint32_t offset, int origin);
+int      ZCALLBACK fclose_mem_func(voidpf opaque, voidpf stream);
+int      ZCALLBACK ferror_mem_func(voidpf opaque, voidpf stream);
 
-    voidpf ZCALLBACK fopen_mem_func64_32 OF((
-                                        voidpf opaque,
-                                        const void* filename,
-                                        int mode));
+typedef struct ourmemory_s {
+    char *base;          /* Base of the region of memory we're using */
+    uint32_t size;       /* Size of the region of memory we're using */
+    uint32_t limit;      /* Furthest we've written */
+    uint32_t cur_offset; /* Current offset in the area */
+    int grow;            /* Growable memory buffer */
+} ourmemory_t;
 
-    uLong ZCALLBACK fread_mem_func OF((
-                                       voidpf opaque,
-                                       voidpf stream,
-                                       void* buf,
-                                       uLong size));
+void fill_memory_filefunc(zlib_filefunc_def* pzlib_filefunc_def, ourmemory_t *ourmem);
 
-    uLong ZCALLBACK fwrite_mem_func OF((
-                                        voidpf opaque,
-                                        voidpf stream,
-                                        const void* buf,
-                                        uLong size));
-
-    long ZCALLBACK ftell_mem_func OF((
-                                      voidpf opaque,
-                                      voidpf stream));
-
-    long ZCALLBACK fseek_mem_func OF((
-                                      voidpf opaque,
-                                      voidpf stream,
-                                      uLong offset,
-                                      int origin));
-
-    int ZCALLBACK fclose_mem_func OF((
-                                      voidpf opaque,
-                                      voidpf stream));
-
-    int ZCALLBACK ferror_mem_func OF((
-                                      voidpf opaque,
-                                      voidpf stream));
-
-    void fill_memory_filefunc64_32 (zlib_filefunc64_32_def* pzlib_filefunc_def);
-
-
-    typedef struct ourmemory_s {
-        void *base; /* Base of the region of memory we're using */
-        uLong size; /* Size of the region of memory we're using */
-        uLong limit; /* Furthest we've written */
-        uLong cur_offset; /* Current offset in the area */
-    } ourmemory_t;
-    
+#ifdef __cplusplus
 }
+#endif
 
 #endif
