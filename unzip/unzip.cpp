a70b40d676144c0a7bdcd4da2691b49cff9dd2de
halx99 <halx99@live.com>
2019-09-24 18:11:00 +0800

    Fix compile error & warning for xcode.

--- unzip/unzip.cpp
+++ unzip/unzip.cpp
@@ -537,7 +537,7 @@ extern unzFile ZEXPORT unzOpen64(const void *path)
 extern unzFile ZEXPORT unzOpenBuffer(const void* buffer, uLong size)
 {
 	zlib_filefunc_def memory_file = { 0 };
-	ourmemory_s oms = { (char*)(buffer), size, size, 0, 0};
+	ourmemory_s oms = { (char*)const_cast<void*>(buffer), static_cast<uint32_t>(size), 0, 0, 0};
 	fill_memory_filefunc(&memory_file, &oms);
 	return unzOpen2(nullptr, &memory_file);
 }
