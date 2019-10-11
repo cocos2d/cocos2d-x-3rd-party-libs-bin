c28f67eb38c431d9e82840a81128510d47e6fd14
HALX99 <halx99@live.com>
2019-10-10 23:52:46 -0700

    Fix issue: https://github.com/cocos2d/cocos2d-x/issues/20182 (#379)

--- unzip/ioapi_mem.cpp
+++ unzip/ioapi_mem.cpp
@@ -56,7 +56,14 @@ voidpf ZCALLBACK fopen_mem_func(voidpf opaque, ZIP_UNUSED const char *filename, 
 
     mem->cur_offset = 0;
 
-    return mem;
+    /* pitfall: The builtin memfs management's behavior is not same with official minizip 1.2.0 */
+    ourmemory_t* memfs = (ourmemory_t*)malloc(sizeof(ourmemory_t));
+    if (memfs != nullptr) {
+        memcpy(memfs, mem, sizeof(ourmemory_t));
+
+        return memfs;
+    }
+    return nullptr;
 }
 
 voidpf ZCALLBACK fopendisk_mem_func(ZIP_UNUSED voidpf opaque, ZIP_UNUSED voidpf stream, ZIP_UNUSED uint32_t number_disk, ZIP_UNUSED int mode)
@@ -144,6 +151,10 @@ long ZCALLBACK fseek_mem_func(ZIP_UNUSED voidpf opaque, voidpf stream, uint32_t 
 int ZCALLBACK fclose_mem_func(ZIP_UNUSED voidpf opaque, ZIP_UNUSED voidpf stream)
 {
     /* Even with grow = 1, caller must always free() memory */
+
+    /* pitfall: The builtin memfs management's behavior is not same with official minizip 1.2.0 */
+    ourmemory_t* mem = (ourmemory_t*)stream;
+    free(mem);
     return 0;
 }
 
