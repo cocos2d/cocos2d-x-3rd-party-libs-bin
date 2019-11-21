8996d34403eed6ccbbfde939737167b6be0a6e4a
dimo4eg <kdl.dima@gmail.com>
2019-11-21 03:14:53 +0200

    luajit

--- lua/luajit/include/luaconf.h
+++ lua/luajit/include/luaconf.h
@@ -102,7 +102,11 @@
 
 /* Note: changing the following defines breaks the Lua 5.1 ABI. */
 #define LUA_INTEGER	ptrdiff_t
-#define LUA_IDSIZE	60	/* Size of lua_Debug.short_src. */
+#if __x86_64__
+  #define LUA_IDSIZE	512	/* Size of lua_Debug.short_src. */
+#else
+  #define LUA_IDSIZE	60	/* Size of lua_Debug.short_src. */
+#endif
 /*
 ** Size of lauxlib and io.* on-stack buffers. Weird workaround to avoid using
 ** unreasonable amounts of stack space, but still retain ABI compatibility.
